e997987e3d1bd57d562de377f7cc10f0979bb398
Miquel de Domingo <miquelddg@gmail.com>
2024-06-01 15:23:45 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aero UI
+
+In progress...
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+  kit: {
+    adapter: adapter()
+  }
+};
+
+export default config;
